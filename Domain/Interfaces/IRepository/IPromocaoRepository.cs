a08c392a777757e29618515fa5dee080d362c005
GhMelo <ghmelo77@gmail.com>
2025-05-28 14:05:03 -0400

    Implementacao Repository Promocao

---
+++ Domain/Interfaces/IRepository/IPromocaoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entity;
+
+namespace Domain.Interfaces.IRepository
+{
+    public interface IPromocaoRepository : IRepository<Promocao>
+    {
+        Promocao obterPorNomePromocao(string nomePromocao);
+    }
+}
---
+++ Infrastructure/Repository/PromocaoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entity;
+using Domain.Interfaces.IRepository;
+
+namespace Infrastructure.Repository
+{
+    public class PromocaoRepository : EFRepository<Promocao>, IPromocaoRepository
+    {
+        public PromocaoRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Promocao obterPorNomePromocao(string nomePromocao)
+            => _dbSet.FirstOrDefault(entity => entity.NomePromocao == nomePromocao);
+    }
+}
